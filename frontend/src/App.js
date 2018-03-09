427cd4c4fbbd63cb99ef1d61e4d8ff9640395808
juanmanuel <jm.dominguez@uniandes.edu.co>
2018-03-09 12:28:42 -0500

    functionalities added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,15 +22,45 @@ class App extends Component {
   submit(e){
     e.preventDefault();
     console.log(this.state.search);
+    let allTags = {};
     if(this.state.search !== ""){
       let baseUrl = "https://instagram.com/explore/tags/" + this.state.search + "/?__a=1";
       fetch(baseUrl)
       .then(response => response.json())
       .then(json =>{
-        let photos = json.graphql.hashtag.edge_hashtag_to_top_spots;
-        console.log(photos);
+        let photos = json.graphql.hashtag.edge_hashtag_to_top_posts.edges;
+        photos.map((photo, index)=>{
+          let tags = photo.node.edge_media_to_caption.edges[0].node.text;
+          let tagArray = tags.split(" ");
+          for(let i = 0; i < tagArray.length; i++){
+            if(tagArray[i].startsWith("#")){
+              if(allTags[tagArray[i]] !== undefined){
+                allTags[tagArray[i]] += 1;
+
+              }
+              else{
+                allTags[tagArray[i]] = 1;
+              }
+            }
+          }
+          
+        })
+        let array = Object.keys(allTags).map(function(k) { return {key: k, value: allTags[k]} });
+        let orderedArray = array.sort(function(a,b){
+          
+          return b.value - a.value;
+        });
+        let plist = [];
+        orderedArray.map(object =>{
+          plist.push(object);
+        });
+        console.log(plist);
+        this.setState({
+          list: plist
+        });
       })
     }
+    
     
   }
   render() {
